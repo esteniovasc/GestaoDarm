e8ce6e69bb56da55eba8d88feda128bbfdb103d7
esteniovasc <esteniovasconcelos@gmail.com>
2024-10-07 21:35:45 -0300

    C4-0.1-SliderUpdateAndHalfFunctional

--- src/components/SliderProjects/SliderProjects.js
+++ src/components/SliderProjects/SliderProjects.js
@@ -1,45 +1,28 @@
 import './SliderProjects.css'
 
+import {Swiper, SwiperSlide} from 'swiper/react'
+
 function SliderProjects() {
+
+    const projects = [
+        { id: '1', image: '/projects/maestro.jpg'},
+        { id: '2', image: '/projects/eyon.jpg'},
+        { id: '3', image: '/projects/sandbox.jpg'},
+        { id: '4', image: '/projects/smile.jpg'}
+    ]
+
     return (
-      <div className='sliderProjects'>
-
-        <div className='slides'>
-
-            <input type="radio" name="radio-btn" id="radio1"/>
-            <input type="radio" name="radio-btn" id="radio2"/>
-            <input type="radio" name="radio-btn" id="radio3"/>
-            <input type="radio" name="radio-btn" id="radio4"/>
-
-            <div className='slideFirst'>
-                <img src="/projects/maestro.jpg" alt="" />
-            </div>
-            <div className='slide'>
-                <img src="/projects/eyon.jpg" alt="" />
-            </div>
-            <div className='slide'>
-                <img src="/projects/sandbox.jpg" alt="" />
-            </div>
-            <div className='slide'>
-                <img src="/projects/smile.jpg" alt="" />
-            </div>
-
-            <div className='navigation-auto'>
-                <div className='auto-btn1'></div>
-                <div className='auto-btn2'></div>
-                <div className='auto-btn3'></div>
-                <div className='auto-btn4'></div>
-            </div>
-
-        </div>
-
-        <div className="manual-navigation">
-            <label for="radio1" className='manual-btn'></label>
-            <label for="radio2" className='manual-btn'></label>
-            <label for="radio3" className='manual-btn'></label>
-            <label for="radio4" className='manual-btn'></label>
-        </div>
-
+      <div className='slider'>
+        <Swiper
+            slidesPerView={1}
+            pagination={{ clickable: true}}
+        >
+            {projects.map((projects)=> (
+                <SwiperSlide key={projects.id}>
+                    <img src={projects.image} alt="a" className='slide-item'/>
+                </SwiperSlide>
+            ))}
+        </Swiper>
       </div>
     );
   }
